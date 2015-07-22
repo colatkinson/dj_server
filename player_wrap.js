93944cb0de63d89114d5560a6b42d6481ac84be6
Colin Atkinson <colin.william.atkinson@gmail.com>
2015-07-22 16:11:26 -0400

    Remove player-related cruft

--- player_wrap.js
+++ player_wrap.js
@@ -1,32 +1,6 @@
-//var Player = require('player');
 var lame = require('lame');
 var Speaker = require('speaker');
 var request = require('request');
-var fs = require('fs');
-var wav = require('wav');
-
-/*module.exports.player = new Player([]).on('error', function(err){
-          // when error occurs
-          console.log(err);
-          });*/
-var debug = console.log;
-/*module.exports.player = new Player('https://ec-media.sndcdn.com/t6djX3HFkvKh.128.mp3?f10880d39085a94a0418a7ef69b03d522cd6dfee9399eeb9a522029b6af1be3d81e7dde268c3142e54f7d9a656c912c268ae1f62cfec2a79d525a81fcfac156a59a11e8059').enable('stream')
-  .on('downloading', function(song) {
-    debug('im downloading... ');
-    debug(song);
-  })
-  .on('playing', function(song) {
-    debug('im playing... ');
-    debug(song);
-  })
-  .on('playend', function(song) {
-    debug('play done, switching to next one ...');
-  })
-  .on('error', function(err) {
-    debug('Opps...!')
-    debug(err);
-  }).play();
-*/
 
 module.exports = {
     _ind: 0,
@@ -40,17 +14,15 @@ module.exports = {
         console.log('play called');
         if(!this.playing && this.playlist.length > this._ind) {
             this.playing = true;
-            //this.stream = request(this.playlist[this._ind]).pipe(new lame.Decoder).pipe(new Speaker);
-            this.stream = request(this.playlist[this._ind]);//.pipe(new lame.Decoder).pipe(fs.createWriteStream('file.wav'));
+            this.stream = request(this.playlist[this._ind]);
+
             var decoder = new lame.Decoder();
             decoder.on('format', function(format) {
-                // console.log(format);
-                //var writer = new wav.Writer(format);
-                //decoder.pipe(writer).pipe(fs.createWriteStream('file.wav'));
                 this.stream = decoder.pipe(new Speaker);
             });
             this.stream.pipe(decoder);
-            console.log(this.stream);
+
+            // Ugly, yet functional
             var parent = this;
             this.stream.on('end', function() {
                 console.log("ended", parent.playing);
@@ -68,7 +40,6 @@ module.exports = {
     },
     next: function() {
         this._ind += 1;
-        console.log(this.playlist[this._ind]);
         this.stop();
         this.play();
     }
--- soundcloud.js
+++ soundcloud.js
@@ -1,6 +1,7 @@
 var request = require('request');
+var secrets = require('./secrets');
 
-var clientId = '***REMOVED***';
+var clientId = secrets.clientId;
 
 function resolveUrl(url, callback) {
     request({url: 'http://api.soundcloud.com/resolve?url=' + url +'&client_id=' + clientId}, function(error, response, body) {
@@ -14,10 +15,7 @@ function getStreamUrl(url, callback) {
     resolveUrl(url, function(data) {
         request(data.uri + '?client_id=' + clientId, function(error, response, body) {
             var json = JSON.parse(body);
-            // callback(json.stream_url + '?client_id=' + clientId);
-            request.head(json.stream_url + '?client_id=' + clientId).on('response', function(response) {
-                callback(response.request.uri.href);
-            });
+            callback(json.stream_url + '?client_id=' + clientId);
         });
     });
 }
